c59b3a9665fdf406381c2bb14931fd78fd972767
manya0033 <manyamahajan3003@gmail.com>
2026-09-10 16:08:05 +1000

    feat: add admin book CRUD API

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -36,7 +36,94 @@ const getBooks = async (req, res) => {
     }
 };
 
-// Exporting the function so our route file can use it
+// Add a new library book
+const createBook = async (req, res) => {
+    try {
+        const book = new Book({
+            title: req.body.title,
+            author: req.body.author,
+            genre: req.body.genre,
+            available: req.body.available
+        });
+
+        const savedBook = await book.save();
+
+        res.status(201).json({
+            message: 'Book added successfully',
+            book: savedBook
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: 'Unable to add book',
+            error: error.message
+        });
+    }
+};
+
+// Update an existing library book
+const updateBook = async (req, res) => {
+    try {
+        const updatedBook = await Book.findByIdAndUpdate(
+            req.params.id,
+            {
+                title: req.body.title,
+                author: req.body.author,
+                genre: req.body.genre,
+                available: req.body.available
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+
+        if (!updatedBook) {
+            return res.status(404).json({
+                message: 'Book not found'
+            });
+        }
+
+        res.status(200).json({
+            message: 'Book updated successfully',
+            book: updatedBook
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: 'Unable to update book',
+            error: error.message
+        });
+    }
+};
+
+// Delete a library book
+const deleteBook = async (req, res) => {
+    try {
+        const deletedBook = await Book.findByIdAndDelete(req.params.id);
+
+        if (!deletedBook) {
+            return res.status(404).json({
+                message: 'Book not found'
+            });
+        }
+
+        res.status(200).json({
+            message: 'Book deleted successfully'
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: 'Unable to delete book',
+            error: error.message
+        });
+    }
+};
+
+// Exporting the functions so our route file can use them
 module.exports = {
-    getBooks
+    getBooks,
+    createBook,
+    updateBook,
+    deleteBook
 };
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-const { getBooks } = require('../controllers/bookController');
+const {
+    getBooks,
+    createBook,
+    updateBook,
+    deleteBook
+} = require('../controllers/bookController');
 
 // This route gets all books or searches books using ?search=
 router.get('/', getBooks);
+
+// Add a new library book
+router.post('/', createBook);
+
+// Update an existing library book
+router.put('/:id', updateBook);
+
+// Delete an existing library book
+router.delete('/:id', deleteBook);
 
 // Exporting the router so server.js can use it
 module.exports = router;
